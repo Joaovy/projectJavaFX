e071a53404b65ea3632cec1576d7dfa6da32df9c
João Antonio <joao.vison.ae@gmail.com>
2025-05-05 15:27:09 -0300

    Tratando com eventos

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/sample.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/View.fxml"));
             Parent parent = loader.load(); // Aqui você carrega o FXML
             Scene scene = new Scene(parent);
             stage.setScene(scene);
